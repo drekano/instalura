cdb998c0154ecca4a02944a7116353d183015d38
Andre Kano <andrekano1@gmail.com>
2021-02-17 00:19:34 +0900

    Aula05: Adicionado footer e GlobalStyle.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import styled from 'styled-components';
 import Menu from '../src/components/commons/Menu';
-
+import Footer from '../src/components/commons/Footer';
 
 export default function Home() {
-  return <div>
-    <Menu />
+  return (
+    <div style={{
+      flex: '1',
+      display: 'flex',
+      flexWrap: 'wrap',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+    }}>
+      <Menu />
+      <Footer />
   </div>
+  )
 }
