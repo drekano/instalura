1963e415b0399dbf9999ff7956f314ceb3537be5
Andre Kano <andrekano1@gmail.com>
2022-02-05 21:12:13 +0900

    fet: Module 3 Aula 1.

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -1,38 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 import get from 'lodash/get';
-import TextStyleVariants from '../../foundation/Text';
+import { TextStyleVariantsMap } from '../../foundation/Text';
 import breakpointsMedia from '../../../theme/utils/breakpointsMedia';
 import propToStyle from '../../../theme/utils/propToStyle';
+import Link from '../Link';
 
 const ButtonGhost = css`
-  color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
-  background-color: transparent;
+  color: ${(props) => get(props.theme, `colors.${props.variant}.color`)};
+  background: transparent; 
 `;
 
 const ButtonDefault = css`
-  color: ${({ theme, variant }) => get(theme, `colors.${variant}.contrastText`)};
-  background-color: ${({ theme, variant }) => get(theme, `colors.${variant}.color`)};
+  color: white;
+  background-color: ${(props) => get(props.theme, `colors.${props.variant}.color`)};
+  color: ${(props) => get(props.theme, `colors.${props.variant}.contrastText`)};
 `;
 
-export const Button = styled.button`
+const ButtonWrapper = styled.button`
   border: 0;
   cursor: pointer;
   padding: 12px 26px;
   font-weight: bold;
   opacity: 1;
+  border-radius: 8px;
+  ${TextStyleVariantsMap.smallestException}
+  ${(props) => {
+    if (props.ghost) {
+      return ButtonGhost;
+    }
+    return ButtonDefault;
+  }}
   transition: opacity ${({ theme }) => theme.transition};
-  border-radius: ${({ theme }) => theme.borderRadius};
-  
+  border-radius: ${(props) => props.theme.borderRadius};
+  &:hover,
+  &:focus {
+    opacity: .5;
+  }
   ${breakpointsMedia({
     xs: css`
-      ${TextStyleVariants.smallestException}
+      /* All devices */
+      ${TextStyleVariantsMap.smallestException}
     `,
     md: css`
-      padding: 12px 43px;
-      ${TextStyleVariants.paragraph1}
+     /* From md breakpoint */
+     ${TextStyleVariantsMap.paragraph1}
     `,
   })}
-
   &:disabled {
     cursor: not-allowed;
     opacity: .2;
@@ -40,15 +55,24 @@ export const Button = styled.button`
   ${({ fullWidth }) => fullWidth && css`
     width: 100%;
   `};
-
   ${propToStyle('margin')}
   ${propToStyle('display')}
-
-  ${({ ghost }) => (ghost ? ButtonGhost : ButtonDefault)}
-  &:hover,
-  &:focus {
-    opacity: .5;
-  }
 `;
 
-export default Button;
+export const Button = ({ href, ...props }) => {
+  const isLink = Boolean(href);
+  const componentTag = isLink ? Link : 'button';
+
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <ButtonWrapper as={componentTag} href={href} {...props} />
+  );
+};
+
+Button.defaultProps = {
+  href: undefined,
+};
+
+Button.propTypes = {
+  href: PropTypes.string,
+};
